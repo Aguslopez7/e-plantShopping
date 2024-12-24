794ecc09a098b837a884383389ba59681a815b59
Aguslopez7 <aln71198@gmail.com>
2024-12-23 23:06:20 -0500

    Landing page styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,42 @@ import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
+import { PiPlant } from "react-icons/pi";
 
 function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
 
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
+    const [showProductList, setShowProductList] = useState(false);
 
-  return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+    const handleGetStartedClick = () => {
+        setShowProductList(true);
+    };
 
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
-      </div>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+                <div className="background-image"></div>
+                <div className="content">
+                    <div className="landing_content">
+                        <PiPlant fontSize={100} color='#88a65e'/>
+                        <h1>Welcome To Paradise Nursery</h1>
+                        <div className="divider"></div>
+                        <p className='slogan'>Where <span style={{color:'#88a65e', fontWeight:'bold'}}>Green</span> Meets Serenity</p>
+
+                        <button className="get-started-button" onClick={handleGetStartedClick}>
+                            Get Started
+                        </button>
+                    </div>
+                    <div className="aboutus_container">
+                        <AboutUs />
+                    </div>
+                </div>
+
+            </div>
+            <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+                <ProductList />
+            </div>
+        </div>
+    );
 }
 
 export default App;
-
-
-
